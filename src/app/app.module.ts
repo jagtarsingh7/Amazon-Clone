4149b1fd9cb3d3287f7a483790e0f6f01f797bd7
jagtar <Jagtar.singh95@yahoo.in>
2023-12-15 11:51:43 +0530

    Auth UI updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
